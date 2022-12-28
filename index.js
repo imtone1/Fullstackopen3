4cb0beca0d22d76b443362303a6923e51d855d40
imtone1 <88165529+imtone1@users.noreply.github.com>
2022-12-28 19:48:44 +0200

    osa3 puhelinluettelo backend 3.2

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  var d = Date(Date.now());
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${d.toString()}</p>`)
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
